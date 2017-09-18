b6f66a2b4056a2e413e2e9b21f99a695757c48e4
ShivaShankariG <30900505+ShivaShankariG@users.noreply.github.com>
2017-09-18 22:23:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,10 +8,10 @@ element.onclick = function() { alert('blah');
 
 var img= document.getElementById("shiva");
 
-img.onClick = function(){
-    alert("Working");
-    img.style.marginLeft='100px';
+img.onclick = function(){alert("HI");
+   img.style.marginLeft='100px'; 
 };
+
 
 /*var img= document.getElementById("shiva");
 

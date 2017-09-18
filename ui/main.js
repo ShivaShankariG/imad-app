648d6ac1d59b208c211827ab6157de390f47c72c
ShivaShankariG <30900505+ShivaShankariG@users.noreply.github.com>
2017-09-18 21:23:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="new- value";
+
+var i=document.getElementById("img");
+i.onClick=function(){
+    i.style.leftmargin='100px';
+};

cd613e2060379f10936ac1af4bba0a91ee8ca628
ShivaShankariG <30900505+ShivaShankariG@users.noreply.github.com>
2017-09-18 21:52:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
 console.log('Loaded!');
-var element=document.getElementById("main-text");
+/*var element=document.getElementById("main-text");
 element.innerHTML="new- value";
 element.onclick = function() { alert('blah'); 
      element.style.marginLeft='100px';
 };
 
 
-var img = document.getElementById("madi");
+var*/ img = document.getElementById("madi");
 img.innerHTML="WHAT THE HELL";
 img.onClick = function(){
     alert("Working");
